7fcdd602e94c21f52a751af62de66e8b47188bf9
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-08-22 11:04:37 -0300

    Updates

---
+++ precache-manifest.ac73e3b97938d4361a0babaf4568c2b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2460e401d3e0a9d04743df0f727cb40e",
+    "url": "/starwars-planets/index.html"
+  },
+  {
+    "revision": "60d911dfa54f13ffbe6b",
+    "url": "/starwars-planets/static/css/main.0c8dd27f.chunk.css"
+  },
+  {
+    "revision": "2cdbdcaa7adba3181b63",
+    "url": "/starwars-planets/static/js/2.bab20030.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planets/static/js/2.bab20030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d911dfa54f13ffbe6b",
+    "url": "/starwars-planets/static/js/main.305d75b1.chunk.js"
+  },
+  {
+    "revision": "d9d282393e64bbdbf77e",
+    "url": "/starwars-planets/static/js/runtime-main.9360541a.js"
+  }
+]);
